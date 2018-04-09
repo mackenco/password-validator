1fe7d9f6e9837e9bee964847a0cb91877ed4255b
Colin <colinwmackenzie@gmail.com>
2018-04-08 21:33:52 -0400

    Updates to handle p flag

--- example/examples.sh
+++ example/examples.sh
@@ -10,3 +10,7 @@ cat ./example/valid.txt | password_validator
 echo "\nwith custom password list"
 echo "running 'echo \"password1,averystrongpassword,password 1\" | password_validator ./example/weak_password_list.txt'"
 echo "password1,averystrongpassword,password 1" | password_validator ./example/weak_password_list.txt
+
+echo "\npasswords passed as arguments"
+echo "running 'password_validator -p password1,abc,averystrongpassword ./example/weak_password_list.txt'"
+password_validator -p password1,abc,averystrongpassword,10000000 ./example/weak_password_list.txt
